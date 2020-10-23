1260c6110cc9dbf806d81f00ff7f9913eceda625
Nikita <Zungarcheg@gmail.com>
2020-10-24 00:11:03 +0300

    Updates

---
+++ precache-manifest.fd84cdb8bc190423d95cf4f4a12d92bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eada44a0e4eb03a2dc30bb83bbdce39",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "91db622fa27d908adcec",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.53c1fbc9.chunk.css"
+  },
+  {
+    "revision": "0352e8959d428611029a",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.31bf44e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.31bf44e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91db622fa27d908adcec",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.d1aa8b09.chunk.js"
+  },
+  {
+    "revision": "0164dcd5ebea52a2d866",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.659f4475.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/goit-react-hw-06-phonebook/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/goit-react-hw-06-phonebook/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
